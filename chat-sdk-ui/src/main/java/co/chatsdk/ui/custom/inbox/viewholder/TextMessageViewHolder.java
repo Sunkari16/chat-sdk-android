9a1c06f014d7ac7ca2479011f814db98a1be7168
vikas kumar <vkbookworm@gmail.com>
2019-05-04 15:50:25 +0530

    fix: added the linkfy

--- chat-sdk-ui/src/main/java/co/chatsdk/ui/custom/inbox/viewholder/TextMessageViewHolder.java
+++ chat-sdk-ui/src/main/java/co/chatsdk/ui/custom/inbox/viewholder/TextMessageViewHolder.java
@@ -1,11 +1,14 @@
 package co.chatsdk.ui.custom.inbox.viewholder;
 
 import android.app.Activity;
+import android.util.Patterns;
 import android.view.View;
 
-import co.chatsdk.ui.custom.inbox.BaseMessageViewHolder;
-
 import co.chatsdk.core.dao.Message;
+import co.chatsdk.ui.custom.inbox.BaseMessageViewHolder;
+import co.chatsdk.ui.custom.inbox.utils.Utility;
+import co.chatsdk.ui.utils.InternalLinkMovementMethod;
+import timber.log.Timber;
 
 public class TextMessageViewHolder extends BaseMessageViewHolder {
 
@@ -16,8 +19,19 @@ public class TextMessageViewHolder extends BaseMessageViewHolder {
     @Override
     public void setMessage(Message message) {
         super.setMessage(message);
-
         messageTextView.setText(message.getText() == null ? "" : message.getText());
+        InternalLinkMovementMethod internalLinkMovementMethod = new InternalLinkMovementMethod(new InternalLinkMovementMethod.OnLinkClickedListener() {
+            @Override
+            public boolean onLinkClicked(String url) {
+                if (Patterns.WEB_URL.matcher(url).matches()) {
+                    Utility.openLinksInChrome(activity, url);
+                    return true;
+                }
+                Timber.v("comment link clicked");
+                return false;
+            }
+        });
+        messageTextView.setMovementMethod(internalLinkMovementMethod);
         setBubbleHidden(false);
         setTextHidden(false);
     }
